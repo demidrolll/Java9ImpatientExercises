28ee04de7b2fb3ba613ab508a0e123fa961ca8dc
ida <demi-drolll@yandex.ru>
2020-11-04 17:03:07 +0300

    Chapter 9, task 1

---
+++ src/main/java/org/demidrolll/java9impatient/ch9/task1/App.java
@@ -0,0 +1,38 @@
+package org.demidrolll.java9impatient.ch9.task1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * Write a utility method for copying all of an InputStream to an OutputStream,
+ * without using any temporary files. Provide another solution, without a
+ * loop, using operations from the Files class, using a temporary file.
+ */
+public class App {
+    public static void main(String[] args) {
+        try (InputStream in = Files.newInputStream(Paths.get("alice.txt"), StandardOpenOption.READ);
+             OutputStream out = Files.newOutputStream(Paths.get("alice_copy.txt"), StandardOpenOption.CREATE)) {
+            // copy(in, out);
+            // copyFiles(in, out);
+            // copyTemp(in, out);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void copy(InputStream in, OutputStream out) throws IOException {
+        in.transferTo(out);
+    }
+
+    public static void copyFiles(InputStream in, OutputStream out) {
+    }
+
+    public static void copyTemp(InputStream in, OutputStream out) {
+    }
+
+}
