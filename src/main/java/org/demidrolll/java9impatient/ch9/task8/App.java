c70c96db3a070ae60f0c5c6a4404cea770e3150d
ida <demi-drolll@yandex.ru>
2020-12-16 15:34:41 +0300

    Chapter 9, task 8

---
+++ src/main/java/org/demidrolll/java9impatient/ch9/task8/App.java
@@ -0,0 +1,13 @@
+package org.demidrolll.java9impatient.ch9.task8;
+
+/**
+ * Write a utility method for producing a ZIP file containing all files from
+ * a directory and its descendants.
+ */
+public class App {
+    public static void main(String[] args) {
+    }
+
+    public static void zipDirectory(String dir, String zipName) {
+    }
+}
