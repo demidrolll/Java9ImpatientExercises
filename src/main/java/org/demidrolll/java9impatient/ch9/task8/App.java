8e57b54733ece40d2b0a3eae9b2bd8f5587b5e9f
ida <demi-drolll@yandex.ru>
2020-12-17 11:13:01 +0300

    Chapter 9, task 8

--- src/main/java/org/demidrolll/java9impatient/ch9/task8/App.java
+++ src/main/java/org/demidrolll/java9impatient/ch9/task8/App.java
@@ -1,13 +1,45 @@
 package org.demidrolll.java9impatient.ch9.task8;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.stream.Stream;
+
 /**
  * Write a utility method for producing a ZIP file containing all files from
  * a directory and its descendants.
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException {
+        zipDirectory("d:\\test", "books.zip");
     }
 
-    public static void zipDirectory(String dir, String zipName) {
+    public static void zipDirectory(String dir, String zipName) throws URISyntaxException {
+        Path arch = Paths.get(zipName);
+        URI uri = new URI("jar", arch.toUri().toString(), null);
+        try (FileSystem zipfs = FileSystems.newFileSystem(uri, Map.of("create", "true"))) {
+            Path zipRoot = zipfs.getPath("/");
+            Stream<Path> files = Files.walk(Paths.get(dir));
+            files
+                    .filter(Files::isRegularFile)
+                    .forEach(src -> {
+                        Path dst = src.getRoot().relativize(src);
+                        dst = zipRoot.resolve(zipfs.getPath(dst.toString()));
+                        try {
+                            Files.createDirectories(dst.getParent());
+                            Files.copy(src, dst);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
