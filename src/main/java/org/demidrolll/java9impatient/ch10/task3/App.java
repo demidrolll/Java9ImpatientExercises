649e4494e4c6d6227803897eb12ef75d5abe884e
ida <demi-drolll@yandex.ru>
2021-02-11 09:38:13 +0300

    Chapter 10, task 3

--- src/main/java/org/demidrolll/java9impatient/ch10/task3/App.java
+++ src/main/java/org/demidrolll/java9impatient/ch10/task3/App.java
@@ -2,10 +2,14 @@ package org.demidrolll.java9impatient.ch10.task3;
 
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.concurrent.Executor;
+import java.util.List;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * Implement a method yielding a task that reads through all words in a
@@ -17,36 +21,51 @@ import java.util.concurrent.Executors;
 public class App {
     public static void main(String[] args) {
 
-        String searchWord = "test";
-        Executor executor = Executors.newCachedThreadPool();
+        String word = "metadata_access_type";
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
+
         try {
-            Files
+            List<Runnable> tasks = Files
                     .walk(Paths.get("c:\\wildfly\\wildfly-18.0.1.Final\\standalone\\log\\"))
                     .filter(Files::isRegularFile)
-                    .forEach(path ->
-                            executor.execute(() -> {
-                                System.out.println("start read file " + path);
-                                if (Thread.currentThread().isInterrupted()) {
-                                    System.out.println("Thread for file " + path + " is interrupted");
-                                    return;
-                                }
-                                try {
-                                    boolean existWord = Files
-                                            .lines(path, StandardCharsets.UTF_8)
-                                            .flatMap(line -> Arrays.stream(line.split("[\\s\\t\\n\\r\\f]")))
-                                            .anyMatch(searchWord::equalsIgnoreCase);
-                                    if (existWord) {
-                                        Thread.currentThread().interrupt();
-                                    }
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                                System.out.println("done read file " + path);
-                            })
-                    );
+                    .map(path -> App.searchWord(path, word))
+                    .collect(Collectors.toList());
+
+            for (Runnable task : tasks) {
+                executor.schedule(task, 1, TimeUnit.SECONDS);
+                if (executor.isTerminated()) {
+                    break;
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        System.out.println("stop program");
+        executor.shutdown();
+    }
+
+    private static Runnable searchWord(Path path, String word) {
+        return () -> {
+            System.out.println("start read file " + path);
+            if (Thread.currentThread().isInterrupted()) {
+                System.out.println("Thread for file " + path + " is interrupted");
+                return;
+            }
+            try {
+                boolean existWord = Files
+                        .lines(path, StandardCharsets.UTF_8)
+                        .flatMap(line -> Arrays.stream(line.split("[\\s\\t\\n\\r\\f]")))
+                        .anyMatch(word::equalsIgnoreCase);
+                if (existWord) {
+                    System.out.println("Word was found");
+                    Thread.currentThread().interrupt();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("done read file " + path);
+        };
     }
 
 }
